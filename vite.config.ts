3f28e1e607c81e40fd53ce5853b846aac56852ea
chifrica <chikaonwunali2019@gmail.com>
2026-01-18 16:00:13 +0100

    chore: Update Vite base path to relative, change favicon to custom logo, and capitalize page title.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // 👈 EXACT repo name
+  base: "./", // 👈 EXACT repo name
   plugins: [react()],
 })
